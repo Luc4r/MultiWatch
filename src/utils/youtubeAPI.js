72ff0ff116405c93fab9d50172594610cbbad5be
Luc4r <fhlucar@gmail.com>
2020-02-05 23:47:43 +0100

    youtubeAPI update
    Added JSDoc comment to avoid future problems with function return

--- src/utils/youtubeAPI.js
+++ src/utils/youtubeAPI.js
@@ -1,33 +1,16 @@
-const getChannelIDAndName = async (enteredID) => {
-  // get channelID - needed to find the livestream
-  const API = 'https://www.googleapis.com/youtube/v3/channels?part=snippet&forUsername=';
-  const APIID = 'https://www.googleapis.com/youtube/v3/channels?part=snippet&id=';
-  const KEY = '&key=AIzaSyAckbMFR-zOKefEnGSWGbiESpHl81VNOYc';
-  let channelID = "";
-  let channelName = "";
-
-  await fetch(API + enteredID + KEY)
+/**
+ * Get youtube channel data from firebase server
+ * @param {string} enteredChannel
+ * @returns {{ error:number, errorMessage:string }}
+ * @returns {{ channelID:string, channelName:string, customUrl:string }}
+ */
+const getChannelIDAndName = async (enteredChannel) => {
+  const firebaseFunction = "https://us-central1-multiwatch-267321.cloudfunctions.net/getChannelNameAndID";
+  const result = await fetch(`${firebaseFunction}?channel=${enteredChannel}`)
     .then(response => response.json())
-    .then(async (dataName) => {
-      if (dataName.pageInfo.totalResults !== 0) {
-        // user entered channel name
-        const channelData = dataName.items[0];
-        channelID = channelData && channelData.id 
-        channelName = channelData.snippet.customUrl && channelData.snippet.customUrl;
-      } else {
-        await fetch(APIID + enteredID + KEY)
-          .then(response => response.json())
-          .then(dataID => {
-            if (dataID.pageInfo.totalResults !== 0 && dataID.items[0].snippet.customUrl) {
-              // user entered channel ID
-              channelID = dataID.items[0].id;
-              channelName = dataID.items[0].snippet.customUrl;
-            }
-          });
-      }
-    });
-    
-  return { channelID, channelName };
+    .catch(error => console.error(error.message));  // eslint-disable-line
+
+  return result;
 };
 
 export default getChannelIDAndName;
